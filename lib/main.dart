dc77a65139e004d0ef32a513f3bf1a8b0978ba9e
joonto <51508535+joontohub@users.noreply.github.com>
2020-09-28 23:35:12 +0900

    test

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'loginpage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'login page',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
